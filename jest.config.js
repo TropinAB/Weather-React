c0038c61dd4ac76b678d58c99cfdabdb9412c628
Andrey Tropin <TropinAB@sibur.ru>
2026-02-08 21:33:15 +0300

    Fix jest.config

--- jest.config.js
+++ jest.config.js
@@ -173,7 +173,7 @@ const config = {
   // The glob patterns Jest uses to detect test files
   testMatch: [
   //   "**/__tests__/**/*.?([mc])[jt]s?(x)",
-    "src/**/?(*.)+(spec|test).?([mc])[jt]s?(x)",
+    "**/?(*.)+(spec|test).?([mc])[jt]s?(x)",
     // "<rootDir>/src/**/*.test.{js,jsx,ts,tsx}",
   ],
 
